913c4d2621e1c80594a7df1f98a60f277a23d0d0
Josh Gantt <29784250+joshgantt@users.noreply.github.com>
2019-09-18 10:11:52 -0400

    working with excel

---
+++ excelread/readfromexcel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import pyexcel
+
+varfilename = input('What is the name of the file? Do not include the extension: ')
+
+myexcelval = {}
+varfilename = varfilename + ".xls"
+
+excelrecords = pyexcel.iget_records(file_name=varfilename)
+
+for x in excelrecords:
+    totalsocket = x['ip'] + ":" + str(x['port'])
+    myexcelval.update({x['service'] : totalsocket})
+
+print(myexcelval)
